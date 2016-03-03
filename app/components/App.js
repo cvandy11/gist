3df588275959f3db31505c5284f29ca744dfe4de
Nick Nestor <nick.nestor@me.com>
2016-03-02 19:48:02 -0500

    Tracking App.js and index.tpl.html changes.

--- app/components/App.js
+++ app/components/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import LiveMap from './LiveMap.js';
+import LayerPanel from './LayerPanel.js';
 import {initSocket} from '../actions/Connect.js';
 
 import {store} from '../store.js';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        store.dispatch(initSocket());
-    }
+	constructor(props) {
+		super(props);
+		store.dispatch(initSocket());
+	}
 
-    render() {
-        return (
-            <div>
-                <LiveMap />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<LiveMap />
+				<LayerPanel />
+			</div>
+		)
+	}
 }
 
 export default App;
