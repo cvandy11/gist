3d4a709cd8f7e3f293ad7b5501b963c286e9bee5
David <acpufreak@yahoo.com>
2016-03-14 17:04:00 -0400

    followed format of LayerPanel.js to make the base tool panel still need to add tools and other things

--- app/components/ToolPanel.js
+++ app/components/ToolPanel.js
@@ -1,1 +1,23 @@
-//
+//Tool Panel
+
+import React from 'react';
+
+//Import Tools and info
+
+//Class
+
+class ToolPanel extends React.Componenet {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		//still needs to have things inside of it
+		return(
+			<div id="tool_panel">
+			</div>
+		}
+	}
+}
+
+export default ToolPanel
