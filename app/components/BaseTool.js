e0b3ede185ec2541ff2e80cefe0cc34f7a831ddc
David <acpufreak@yahoo.com>
2016-03-15 20:04:43 -0400

    added a glyphicon and a popover for demo purposes

--- app/components/BaseTool.js
+++ app/components/BaseTool.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 //boostrap imports
-import {Button} from 'react-bootstrap';
+import {Button, OverlayTrigger, Popover} from 'react-bootstrap';
 
 class BaseTool extends React.Component{
 
@@ -15,13 +15,22 @@ class BaseTool extends React.Component{
 		this.setState({
 			name: "Base Tool",
 			active: true,
-			bsStyle:"primary"
+			bsStyle:"primary",
+			glyph: "glyphicon glyphicon-fire"
 		});
 	}
 	
 	render(){
 		return(
-			<Button bsStyle = {this.state.bsStyle} id="base_tool"> {this.state.name} </Button>
+			<OverlayTrigger trigger="click" placement="right" overlay={
+				<Popover title="Example Popover">
+					<strong>This Does Nothing Yet </strong>
+				</Popover>
+			}>
+				<Button bsStyle = {this.state.bsStyle} id="base_tool">
+					<span className={this.state.glyph}></span> {this.state.name}
+				 </Button>
+			</OverlayTrigger>
 		);
 	}
 }
