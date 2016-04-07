2f6872012d27a6830515ca4042b8632b3fa99674
Chris Vandenberg <cvandy11@live.com>
2016-04-07 15:44:30 -0400

    added reducer for error messages

---
+++ app/reducers/error.js
@@ -0,0 +1,24 @@
+import {ERROR} from '../actions/Connect.js';
+
+export default function errorReducer(previousControlState, action) {
+
+    if(previousControlState == undefined) {
+        return {
+            type: "none",
+            message: "",
+        };
+    };
+
+    const newState = Object.assign({}, previousControlState);
+
+    switch(action.type) {
+        case ERROR:
+            newState.type = "error";
+            newState.message = action.message;
+            break;
+        default:
+            break;
+    };
+
+    return newState;
+}
